f46204d65a8eeae6fc0d30d8a75c2bd312fc5af5
MARXCBR <824368602@qq.com>
2019-06-14 11:04:20 +0800

    随意修改了一部分内容 尝试用命令行映射，失败

--- NetDrive/NetDrive/NetDrive.cpp
+++ NetDrive/NetDrive/NetDrive.cpp
@@ -1,13 +1,8 @@
 #include "NetDrive.h"
-#pragma comment(lib, "mpr.lib")
 #include <qdebug.h>
-#include <windows.h>
-#include <tchar.h>
-#include <WinNetWk.h>
-#include <stdio.h>
-#include <string>
-#include <iostream>
-#pragma comment(lib, "Mpr.lib")
+
+#include <qprocess.h>
+
 
 using namespace std;
 
@@ -37,52 +32,5 @@ NetDrive::NetDrive(QWidget *parent)
 {
 	ui.setupUi(this);
 
-
-
-		string szPasswd = "marx", szUserName = "admin"; //用户名和密码
-		NETRESOURCE net_Resource;
-		memset(&net_Resource, 0, sizeof(net_Resource));
-		net_Resource.dwDisplayType = RESOURCEDISPLAYTYPE_DIRECTORY;
-		net_Resource.dwScope = RESOURCE_CONNECTED;
-		net_Resource.dwType = RESOURCETYPE_ANY;
-		net_Resource.dwUsage = 0;
-		net_Resource.lpComment = TEXT("");
-		net_Resource.lpLocalName = TEXT("H:");  //映射成本地驱动器Z:
-		net_Resource.lpProvider = NULL;
-		net_Resource.lpRemoteName = TEXT("\\\\192.168.1.103\\photo"); // \\servername\共享资源名
-		DWORD dwFlags = CONNECT_UPDATE_PROFILE;
-
-		LPCWSTR pass ;
-		pass = s2ws(szPasswd);
-		LPCWSTR user ;
-		user = s2ws(szUserName);
-
-		DWORD dw = WNetAddConnection2(&net_Resource, pass, user, dwFlags);
-		if (dw == ERROR_ALREADY_ASSIGNED) {
-			
-		}
-		else if (dw == ERROR_DEVICE_ALREADY_REMEMBERED) {
-			
-		}
-		else if (dw != NO_ERROR) {
-			
-		}
-		else if (dw == ERROR_SUCCESS)
-		{
-			//这里可以像操作本地磁盘一样操作文件了，上面映射为Z盘，就以写文件为例
-			FILE *f = fopen("h:\\123.txt", "w");
-			if (f != NULL)
-			{
-				fwrite("12313", 5, 1, f);
-				fclose(f);
-			}
-			//Sleep(100);
-			////断开共享连接
-			//DWORD dwConFlig = WNetCancelConnection2(net_Resource.lpLocalName, CONNECT_UPDATE_PROFILE, true);
-			//if (dwConFlig != ERROR_SUCCESS)
-			//{
-			//	
-			//}
-		}
-
 }
+
--- NetDrive/NetDrive/NetDrive.h
+++ NetDrive/NetDrive/NetDrive.h
@@ -2,6 +2,18 @@
 
 #include <QtWidgets/QMainWindow>
 #include "ui_NetDrive.h"
+#pragma comment(lib, "mpr.lib")
+#include <string>
+#include <windows.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <string.h>
+#include <psapi.h>
+#include <strsafe.h>
+#include <ctime> 
+#include <sstream>
+#include <vector>
+#include <fstream>
 
 class NetDrive : public QMainWindow
 {
@@ -9,7 +21,10 @@ class NetDrive : public QMainWindow
 
 public:
 	NetDrive(QWidget *parent = Q_NULLPTR);
+	void MappingDrive();
+	DWORD setMapping(char driveLetter, string remoteShare, NETRESOURCE & nr, DWORD dwFlags);
 
 private:
 	Ui::NetDriveClass ui;
+
 };
